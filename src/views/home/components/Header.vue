0c8400482b30ec65a286343565b0af8e491f5ede
max <g2498727035@gmail.com>
2020-01-09 11:26:31 +0800

    首页header组件

---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HomeHeader'
+}
+</script>
+<style scoped>
+  .header {
+      display: flex;
+      line-height: .86rem;
+      background: #00bcd4;
+      color: #ffffff;
+  }
+  .header-left  {
+      width: 0.64rem;
+      float: left;
+  }
+  .header-input {
+      flex: 1;
+      height: 0.64rem;
+      line-height: 0.64rem;
+      margin-top: .12rem;
+      margin-left: .12rem;
+      background: #ffffff;
+      border-radius: 0.1rem;
+      color: #ccc;
+      padding-left: .2rem;
+  }
+    .header-right {
+        width: 1.24rem;
+        float: right;
+        text-align: center;
+    } 
+    .back-icon {
+        text-align: center;
+        font-size: .4rem;
+    } 
+    .arrow-icon {
+        font-size: .2rem;
+    } 
+</style>
