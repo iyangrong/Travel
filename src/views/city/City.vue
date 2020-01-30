3940f895566bb625f898d8bb995ae071d68c0992
max <g2498727035@gmail.com>
2020-01-30 22:54:39 +0800

    city ajax

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,11 +2,13 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 <script>
+import axios from 'axios'
+
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
@@ -19,6 +21,28 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data() {
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    mounted() {
+        this.getCityInfo()
+    },
+    methods: {
+        getCityInfo() {
+            axios.get('/static/mock/city.json')
+            .then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc(res) {
+            res = res.data
+            if(res.ret && res.data) {
+                this.cities = res.data.cities
+                this.hotCities = res.data.hotCities
+            }
+        }
     }
 }
 </script>
