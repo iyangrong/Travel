476858c85f2d1fff9b60d515c4d713c28d9882c8
max <g2498727035@gmail.com>
2020-01-30 16:32:36 +0800

    city router

---
+++ src/views/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to='/'>
+            <div class="iconfont header-icon">&#xe624;</div>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'cityHeader'
+}
+</script>
+<style scoped>
+    .header {
+        overflow: hidden;
+        line-height: 0.86rem;
+        height: 0.86rem;
+        text-align: center;
+        color: #fff;
+        font-size: 0.4rem;
+        background-color: #00bcd4;
+        position: relative;
+    }
+    .header-icon {
+        width: 0.64rem;
+        text-align: center;
+        font-size: 0.4rem;
+        position: absolute;
+        top: 0;
+        left: 0;
+        color: #fff;
+    }
+</style>
